93da2c91fbaa09aff3fa518ef48abece7ff3598d
heitor <heitorcasella@gmail.com>
2023-02-12 21:25:43 -0300

    Lib de tradução de Validations, Criação de Migrations e outras configurações

---
+++ database/migrations/2023_02_12_235256_create_categorias_produtos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('descricao');
+            $table->datetime('criado_em');
+            $table->datetime('atualizado_em');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias_produtos');
+    }
+};
